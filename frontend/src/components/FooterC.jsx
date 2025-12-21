4bc2f7e77d5b437e8d760582464a8cde9ae317b2
bhabanaagrawal1 <bhabanaagrawal1111@gmail.com>
2025-12-22 00:33:46 +0530

    Adding the contact page

---
+++ frontend/src/components/FooterC.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+const FooterC = () => {
+  const icons = [<i class="ri-linkedin-box-fill"></i>,<i class="ri-twitter-x-line"></i>,<i class="ri-instagram-fill"></i>,<i class="ri-youtube-fill"></i>]
+  const links = ["https://www.linkedin.com/in/bhabana-agrawal-6a6313358/","https://x.com/AgrawalBha9","https://www.instagram.com/","https://www.youtube.com/"]
+  const heading = ["About","Company","Support","Get in Touch"]
+  const footer = {
+    1:["About Us","Recipe","Download","Contact"],
+    2:["Out Recipe","Subscribe Us","FAQ","Deals"],
+    3:["Account","Support Center","Feedback","Accessibility"],
+    4:["4517 Washington Ave.","Manchester,","Kentucky 39495","Mail Us"],
+  };
+  return (
+    <div>
+      <style>
+        {`
+        @import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap');
+          .logo_i{
+              font-family: "Dancing Script", cursive;
+          }
+        `}
+      </style>
+      <div className='w-full h-[80vh] bg-gray-100 overflow-hidden'>
+        <div className='w-full h-[50%] flex justify-around items-center'>
+          <div className='w-[30%] h-[90%] flex justify-center items-center'>
+            <div className='w-[90%]'>
+              <h1 className='text-6xl logo_i m-0'>GreenCart</h1>
+              <h3 className='my-2.5 text-[14px]'>Every product carries a little more care than expected, from our care to your table.</h3>
+              <div className='flex justify-start gap-3'>
+                {icons.map((item,index) =>(
+                   <NavLink key={index} to={links[index]}>
+                      <div className="w-14 h-14 rounded-full shadow-sm bg-white flex justify-center items-center text-xl hover:text-[24px]">{item}</div>
+                   </NavLink>
+                ))}
+              </div>
+            </div>
+          </div>
+          {heading.map((item,index)=>(
+            <div key={index} className='w-[15%] h-[90%] flex justify-center items-center'>
+            <div>
+              <h1 className='text-[22px]'>{item}</h1>
+              {footer[index + 1].map((itm,idx)=>(
+                <h3 key={idx} className='text-[14px] p-1.75 text-black-600 hover:cursor-pointer hover:scale-102 hover:font-semibold hover:underline hover:underline-offset-2'>{itm}</h3>
+              ))}
+            </div>
+          </div>
+          ))}
+        </div>
+        <div className='w-full h-auto'>
+            <h1 className='text-[230px] text-center opacity-50 backdrop-opacity-90'>GreenCart</h1></div>
+      </div>
+    </div>
+  )
+}
+
+export default FooterC
+
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import ContactH from '../components/ContactH'
+import FooterC from '../components/FooterC'
 
 const Contact = () => {
   return (
     <div>
       <Navbar/>
       <ContactH/>
+      <FooterC/>
     </div>
   )
 }
